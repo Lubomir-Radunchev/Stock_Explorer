971dac5eec9bc162371914ea6ea33cafe1cbe583
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-03-30 19:56:40 +0200

    newest migrations

---
+++ Migrations/20240315153317_AddColumNamedDataInStockRecord.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stock_Explorer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumNamedDataInStockRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Data",
+                table: "StockRecords",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "StockRecords");
+        }
+    }
+}
